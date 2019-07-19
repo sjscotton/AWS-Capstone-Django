e41d30d4294b68c717067c8ccebdf0618aba1146
sjscotton <sj.scotton@gmail.com>
2019-07-18 18:01:35 -0700

    added Vote model

--- ivote_api/models.py
+++ ivote_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Vote_Date(models.Model):
+
+    state_voter_id = models.CharField(max_length=15, default='00')
+    county_code = models.CharField(max_length=10, default='00')
+    election_date = models.CharField(max_length=15, default="00")
