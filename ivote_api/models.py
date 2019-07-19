3592fd6a6d715e7ec84f750a10448ff48ff25257
sjscotton <sj.scotton@gmail.com>
2019-07-18 18:02:03 -0700

    rename

--- ivote_api/models.py
+++ ivote_api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Vote_Date(models.Model):
+class Vote(models.Model):
 
     state_voter_id = models.CharField(max_length=15, default='00')
     county_code = models.CharField(max_length=10, default='00')
