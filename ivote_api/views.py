c7b4dda21166dd703c87efd886ebe42ef5c9fbcb
sjscotton <sj.scotton@gmail.com>
2019-07-19 16:13:50 -0700

    added Voting_stats into view

--- ivote_api/views.py
+++ ivote_api/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from ivote_api.models import Vote, Voter
+from ivote_api.models import Vote, Voter, Voting_Stats
 # Create your views here.
 def index(request):
     votes = Vote.objects.all()
     voters = Voter.objects.all()
+    stats = Voting_Stats.objects.all()
     print(voters)
     data = {
       'Hello': 'Friend',
       "number of votes": len(votes),
       "num voter": len(voters),
+      "stats": len(stats)
 
     }
 
