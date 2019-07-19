42218f0868f052af4735ad7502b311ea1800f112
sjscotton <sj.scotton@gmail.com>
2019-07-19 15:24:38 -0700

    removed files

--- ivote_api/views.py
+++ ivote_api/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from ivote_api.models import Vote
+from ivote_api.models import Vote, Voter
 # Create your views here.
 def index(request):
     votes = Vote.objects.all()
+    voters = Voter.objects.all()
+    # print(voters)
     data = {
       'Hello': 'Friend',
-      "number of votes": len(votes)
+      "number of votes": len(votes),
+      # "num voter": len(voters),
 
     }
 
