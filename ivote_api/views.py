5fbd53dff4196f624aea49aa1c5acb618c18a173
sjscotton <sj.scotton@gmail.com>
2019-07-21 16:19:32 -0700

    testing index without connecting to the database

--- ivote_api/views.py
+++ ivote_api/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from ivote_api.models import Vote, Voter, Voting_Stats
+# from ivote_api.models import Vote, Voter, Voting_Stats
 # Create your views here.
+
+
 def index(request):
-    votes = Vote.objects.all()
-    voters = Voter.objects.all()
-    stats = Voting_Stats.objects.all()
-    print(voters)
+    # votes = Vote.objects.all()
+    # voters = Voter.objects.all()
+    # stats = Voting_Stats.objects.all()
+    # print(voters)
     data = {
-      'Hello': 'Friend',
-      "number of votes": len(votes),
-      "num voter": len(voters),
-      "stats": len(stats)
+        'Hello': 'Friend',
+        # "number of votes": len(votes),
+        # "num voter": len(voters),
+        # "stats": len(stats)
 
     }
-
 
     return JsonResponse({"data": data})
