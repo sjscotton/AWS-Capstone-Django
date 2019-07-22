d8e3babb8f44d243e445d55cda8026c84b58f3ff
sjscotton <sj.scotton@gmail.com>
2019-07-21 19:47:27 -0700

    added get_voter view

--- ivote_api/views.py
+++ ivote_api/views.py
@@ -20,3 +20,39 @@ def index(request):
     }
 
     return JsonResponse({"data": data})
+
+
+def get_voter(request):
+
+    first_name = request.GET.get('first_name', None)
+    last_name = request.GET.get('last_name', None)
+    birthdate = request.GET.get('birthdate', None)
+    voter_id = request.GET.get('voter_id', None)
+
+    if voter_id:
+        try:
+            person = Voter.objects.get(state_voter_id=voter_id)
+        except:
+            return JsonResponse({'message': f'Record not found for voter ID: {voter_id}.'}, status=404)
+
+    else:
+        if not first_name or not last_name or not birthdate:
+            return JsonResponse({'message': "Must supply first_name, last_name and birthdate"}, status=400)
+
+        try:
+            person = Voter.objects.get(
+                f_name=first_name.upper(), l_name=last_name.upper(), birthdate=birthdate.replace('-', '/'))
+        except:
+            return JsonResponse({'message': f'Record not found for {first_name} {last_name}.'}, status=404)
+
+    data = {
+        'first_name': person.f_name,
+        'last_name': person.l_name,
+        "middle_name": person.m_name,
+        'voter_id': person.state_voter_id,
+        'address': person.get_address(),
+        'county_code': person.county_code,
+        'city': person.city,
+        'age_group': person.get_age_group()
+    }
+    return JsonResponse(data, status=200)
