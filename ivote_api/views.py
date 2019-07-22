43a10cf0f7c4b7d1445266e5fd36259c761d8d1f
sjscotton <sj.scotton@gmail.com>
2019-07-21 18:36:01 -0700

    test

--- ivote_api/views.py
+++ ivote_api/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# from ivote_api.models import Vote, Voter, Voting_Stats
+from ivote_api.models import Vote, Voter, Voting_Stats
 # Create your views here.
 
 
 def index(request):
-    # votes = Vote.objects.all()
-    # voters = Voter.objects.all()
+    vote = Vote.objects.first()
+    voter = Voter.objects.first()
+    print(vote)
+    print(voter)
     # stats = Voting_Stats.objects.all()
     # print(voters)
     data = {
         'Hello': 'Friend',
-        # "number of votes": len(votes),
-        # "num voter": len(voters),
+        "vote date": vote.election_date,
+        "voter name": voter.f_name,
         # "stats": len(stats)
 
     }
